f2ed1741a9a838cfb7a4edae4b69b81370f79ec3
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2023-11-16 16:30:51 +0100

    feat: :sparkles: cycle through all markdown files in the folder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use serde::Deserialize;
 use std::fs;
+use std::fs::metadata;
+// use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
 use yaml_front_matter::{Document, YamlFrontMatter};
 
 #[derive(Deserialize)]
@@ -13,42 +16,63 @@ struct Metadata {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_path: &str = "/home/andrem/Documents/notes_science/APA/The_predicted_RNA-binding_protein_regulome_of_axonal_mRNAs.md";
+    let dir_path: &str = "/home/andrem/Documents/notes_science/";
+    for entry in WalkDir::new(dir_path)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+    {
+        // turn the DirEntry object into a Path object
+        let path = entry.path();
 
-    let f: String = fs::read_to_string(file_path).expect("Should have been able to read the file");
+        // check if the path is to a directory or to a file that is not a markdown
+        if metadata(path).unwrap().is_dir() {
+            continue;
+        }
+        if !path.to_str().unwrap().ends_with(".md") {
+            continue;
+        }
 
-    let document: Document<Metadata> = YamlFrontMatter::parse::<Metadata>(&f).unwrap();
+        let f: String = fs::read_to_string(path).expect("Should have been able to read the file");
 
-    let Metadata {
-        title,
-        author,
-        journal,
-        year,
-        number,
-        volume,
-    } = document.metadata;
+        let document_result: Result<Document<Metadata>, Box<dyn std::error::Error>> =
+            YamlFrontMatter::parse::<Metadata>(&f);
 
-    // let (author1, ss): (String, Vec<_>) = author.split("and").collect();
-    let authors: Vec<&str> = author.split("and").collect();
-    let first: &str = authors[0].trim();
-    let first: Vec<&str> = first.split(" ").collect();
-    let last_name: &str = first[first.len() - 1];
-    println!("{}", last_name);
+        let document: Document<Metadata> = match document_result {
+            Ok(yaml_front_matter) => yaml_front_matter,
+            Err(_) => continue,
+        };
 
-    let output: String = format!(
-        "@article{{{last_name}{year},
-    title={{{title}}},
-    author={{{author}}},
-    journal={{{journal}}},
-    year={{{year}}},
-    number={{{number}}},
-    volume={{{volume}}}
-    }}"
-    );
+        println!("{}", path.display());
 
-    let out_path: &str = "/home/andrem/Documents/notes_science/test.bib";
+        let Metadata {
+            title,
+            author,
+            journal,
+            year,
+            number,
+            volume,
+        } = document.metadata;
 
-    fs::write(out_path, output).expect("Unable to write file");
-    println!("");
+        let authors: Vec<&str> = author.split("and").collect();
+        let first: &str = authors[0].trim();
+        let first: Vec<&str> = first.split(" ").collect();
+        let last_name: &str = first[first.len() - 1];
+
+        let _output: String = format!(
+            "@article{{{last_name}{year},
+        title = {{{title}}},
+        author = {{{author}}},
+        journal = {{{journal}}},
+        year = {{{year}}},
+        number = {{{number}}},
+        volume = {{{volume}}}
+        }}"
+        );
+
+        let _out_path: &str = "/home/andrem/Documents/notes_science/test.bib";
+
+        // fs::write(out_path, output).expect("Unable to write file.");
+    }
     Ok(())
 }
