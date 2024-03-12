bf1f6e37a6f4feb54c44966ea91b23e9051108c8
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2024-03-12 20:52:50 +0100

    test: :white_check_mark: write tests for get_yaml_front_matter

--- src/front_matter_types.rs
+++ src/front_matter_types.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use yaml_front_matter::{Document, YamlFrontMatter};
 pub mod article_bio_like;
 
-pub fn parse_document_bio(f: String) -> Result<Document<MetadataBio>, Box<dyn Error>> {
+fn parse_document_bio(f: String) -> Result<Document<MetadataBio>, Box<dyn Error>> {
     let parsed_document: Result<Document<MetadataBio>, Box<dyn Error>> =
         YamlFrontMatter::parse::<MetadataBio>(&f);
     parsed_document
@@ -24,7 +24,7 @@ pub fn get_yaml_front_matter(
 mod tests_parse_document_bio {
     use super::*;
 
-    fn test_parse_document_bio_data() -> String {
+    fn test_file_bio_data() -> String {
         let test_f: String = String::from(
             "
 ---
@@ -34,7 +34,7 @@ journal: 3
 year: 4
 volume: 5
 number: 6
-pages: 7
+pages: 7-10
 ---
 
 # test
@@ -46,50 +46,114 @@ this is a test
 
     #[test]
     fn test_title() {
-        let test_f: String = test_parse_document_bio_data();
+        let test_f: String = test_file_bio_data();
         let result: Document<MetadataBio> = parse_document_bio(test_f).unwrap();
         assert_eq!(result.metadata.title, String::from("1"));
     }
 
     #[test]
     fn test_author() {
-        let test_f: String = test_parse_document_bio_data();
+        let test_f: String = test_file_bio_data();
         let result: Document<MetadataBio> = parse_document_bio(test_f).unwrap();
         assert_eq!(result.metadata.author, String::from("2"));
     }
 
     #[test]
     fn test_journal() {
-        let test_f: String = test_parse_document_bio_data();
+        let test_f: String = test_file_bio_data();
         let result: Document<MetadataBio> = parse_document_bio(test_f).unwrap();
         assert_eq!(result.metadata.journal, String::from("3"));
     }
 
     #[test]
     fn test_year() {
-        let test_f: String = test_parse_document_bio_data();
+        let test_f: String = test_file_bio_data();
         let result: Document<MetadataBio> = parse_document_bio(test_f).unwrap();
         assert_eq!(result.metadata.year, 4);
     }
 
     #[test]
     fn test_volume() {
-        let test_f: String = test_parse_document_bio_data();
+        let test_f: String = test_file_bio_data();
         let result: Document<MetadataBio> = parse_document_bio(test_f).unwrap();
         assert_eq!(result.metadata.volume, 5);
     }
 
     #[test]
     fn test_number() {
-        let test_f: String = test_parse_document_bio_data();
+        let test_f: String = test_file_bio_data();
         let result: Document<MetadataBio> = parse_document_bio(test_f).unwrap();
         assert_eq!(result.metadata.number, 6);
     }
 
     #[test]
     fn test_pages() {
-        let test_f: String = test_parse_document_bio_data();
+        let test_f: String = test_file_bio_data();
         let result: Document<MetadataBio> = parse_document_bio(test_f).unwrap();
-        assert_eq!(result.metadata.pages, String::from("7"));
+        assert_eq!(result.metadata.pages, String::from("7-10"));
+    }
+}
+
+#[cfg(test)]
+mod tests_get_yaml_front_matter {
+    use super::*;
+
+    fn test_file_bio_data() -> String {
+        let test_f: String = String::from(
+            "
+---
+title: 1
+author: 2
+journal: 3
+year: 4
+volume: 5
+number: 6
+pages: 7-10
+---
+
+# test
+this is a test
+        ",
+        );
+        test_f
+    }
+
+    #[test]
+    fn test_title() {
+        let test_f: String = test_file_bio_data();
+        let style: &str = "article_bio_like";
+        let result: Document<MetadataBio> = get_yaml_front_matter(test_f, style).unwrap();
+        assert_eq!(result.metadata.title, String::from("1"));
+    }
+
+    #[test]
+    fn test_pages() {
+        let test_f: String = test_file_bio_data();
+        let style: &str = "article_bio_like";
+        let result: Document<MetadataBio> = get_yaml_front_matter(test_f, style).unwrap();
+        assert_eq!(result.metadata.pages, String::from("7-10"));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_missing_title() {
+        let test_f: String = String::from(
+            "
+---
+author: 2
+journal: 3
+year: 4
+volume: 5
+number: 6
+pages: 7-10
+---
+
+# test
+this is a test
+        ",
+        );
+        let style: &str = "article_bio_like";
+        let result: Document<MetadataBio> = get_yaml_front_matter(test_f, style).unwrap();
+        assert_eq!(result.metadata.author, String::from("2")); // remove the ignore after implementing
     }
 }
