c80ff85e840d9bd76eafed0442bc8581d5fbb175
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2024-03-14 14:11:14 +0100

    perf: :zap: improve performance of the main() function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::error::Error;
-use std::fs::{self, metadata, File};
+use std::fs::{self, File};
 use std::io::Write;
 use std::path::Path;
 use std::result::Result;
@@ -20,30 +20,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     // parse CLI arguments
     let args: Args = Args::parse();
 
+    // create/open a file in the output directory before appending each file's info
+    let mut output_handle: File =
+        create_open_output_file(args.output_path.as_str(), args.overwrite);
+
     // cycle the files and directories in the provided path
-    for entry in WalkDir::new(args.input_directory.as_str())
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|entry: Result<DirEntry, walkdir::Error>| entry.ok())
-    {
+    for entry in WalkDir::new(&args.input_directory).follow_links(true) {
         // turn the DirEntry object into a Path object
+        let entry: DirEntry = entry?;
         let path: &Path = entry.path();
 
-        // check if the path is pointing to a directory or to a file that is not a markdown
-        if metadata(path).unwrap().is_dir()
-            || !path.to_str().unwrap().to_lowercase().ends_with(".md")
-        {
+        // Skip directories and non-Markdown files
+        if path.is_dir() || !path.extension().map_or(false, |ext| ext == "md") {
             continue;
         }
 
         // read the file and parse the YAML front matter
         let f: String = fs::read_to_string(path)?;
-        let parsed_document: Result<Document<MetadataBio>, Box<dyn Error>> =
-            get_yaml_front_matter(f, args.style.as_str());
-        let yaml_front_matter: Document<MetadataBio> = match parsed_document {
-            Ok(content) => content,
-            Err(_) => continue,
-        };
+        let yaml_front_matter: Document<MetadataBio> =
+            match get_yaml_front_matter(f, args.style.as_str()) {
+                Ok(content) => content,
+                Err(_) => continue,
+            };
 
         // get the first author's last name to use as the Key in the .bib format
         let last_name: &str = get_first_author_last_name(&yaml_front_matter.metadata.author)?;
@@ -51,14 +49,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         // build the .bib formatted string to write to file
         let lines: Vec<String> = generate_bib_metadata_lines(&yaml_front_matter.metadata);
         let output: String =
-            wrap_metadata_lines(&yaml_front_matter.metadata.year, last_name, lines);
-
-        // create/open a file in the output directory
-        let mut output_handle: File =
-            create_open_output_file(&args.output_path.as_str(), args.overwrite);
+            wrap_metadata_lines(&yaml_front_matter.metadata.year, &last_name, lines);
 
         // append these lines to the file
-        output_handle.write(output.as_bytes())?;
+        output_handle.write_all(output.as_bytes())?;
     }
     Ok(())
 }
